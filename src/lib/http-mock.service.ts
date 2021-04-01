716f94b3a79986b1b17a6fc12143c8910e132669
Дмитрий Шкатов <Dmitriy.Shkatov@waveaccess.ru>
2021-04-01 17:02:02 +0500

    [BF] Change private method as private :)

--- src/lib/http-mock.service.ts
+++ src/lib/http-mock.service.ts
@@ -1,15 +1,15 @@
 import { Observable } from "rxjs/internal/Observable";
 
 export class MockHttpService {
-  min: number
-  max: number;
+  private min: number
+  private max: number;
   
   constructor(param: { minTimeInterval?: number; maxTimeInterval?: number; } = { minTimeInterval: 200, maxTimeInterval: 500 }) {
     this.min = param.minTimeInterval;
     this.max = param.maxTimeInterval;
   }
 
-  randInt = (min:number, max:number) => min + Math.floor((max - min) * Math.random());
+  private randInt = (min:number, max:number) => min + Math.floor((max - min) * Math.random());
 
   get<T>(answer: any): Observable<T> {
     return new Observable<any>(observer => {
